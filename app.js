4ff9a6295b20860d100d587659c160224ece3e9f
huyn3195 <98776668+huyn3195@users.noreply.github.com>
2024-07-24 11:35:06 -0700

    Add functionality

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-
+const posts = [];
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }))
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs",{posts});
 })
 app.get("/about",(req,res)=>{
   res.render("about.ejs");
 })
+app.get("/blog",(req,res)=>{
+  res.render("post.ejs");
+})
+app.post('/blog', (req, res) => {
+  const { title, content } = req.body;
+  posts.push({ title, content });
+  res.redirect('/');
+});
 app.listen(port,()=>{
   console.log(`Server is listening on port ${port}`)
 })
